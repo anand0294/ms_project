b47bb57774bc964ef2525b6ad1726b03d457e55e
anandmohantiwari94@gmail.com <anandmohantiwari94@gmail.com>
2024-08-27 22:14:08 +0530

    PATCH and DELETE API Added

--- 3. Full Stack LLD/rest/index.js
+++ 3. Full Stack LLD/rest/index.js
@@ -1,46 +1,69 @@
 const express = require("express");
-const app=express();
+const app = express();
 const fs = require("fs");
 
 const data = JSON.parse(fs.readFileSync("data.json"));
 
-const products = data.products;
+let products = data.products;
 
 app.use(express.json());
 
 //GET Operations
-app.get('/heartbeat',(req,res)=>{
+app.get("/heartbeat", (req, res) => {
     res.send("App is running fine !!");
 });
 
-app.get('/products',(req,res)=>{
+app.get("/products", (req, res) => {
     res.send(products);
 });
 
-app.get('/products/:productId',(req,res)=>{
+app.get("/products/:productId", (req, res) => {
     const productId = Number(req.params.productId);
-    const product = products.find(product => product.id === productId) || {};
-    if(product.id) {
-    res.send(product);
-     } else {
+    const product = products.find((product) => product.id === productId) || {};
+    if (product.id) {
+        res.send(product);
+    } else {
         res.status(404).send("Product not found !!");
     }
 });
 
 //POST Operations
-app.post("/product",(req,res)=>{
-    products.push({...req.body,id: products.length+1});
+app.post("/product", (req, res) => {
+    products.push({ ...req.body, id: products.length + 1 });
     res.status(201).send("Successfully Added");
 });
 
 //PUT Operations
-app.put("/products/:productId",(req,res)=>{
+app.put("/products/:productId", (req, res) => {
     const productId = Number(req.params.productId);
-    const productIdx = products.findIndex(product => product.id === productId);
-    products.splice(productIdx,1,{...req.body,id: productId});
+    const productIdx = products.findIndex((product) => product.id === productId);
+    products.splice(productIdx, 1, { ...req.body, id: productId });
     res.status(201).send("Successfully Updated !!");
 });
 
-app.listen(8080,()=>{
+//PATCH Operations
+app.patch("/products/:productId", (req, res) => {
+    const productId = Number(req.params.productId);
+    const productIdx = products.findIndex((product) => product.id === productId);
+    const product = products[productIdx];
+    products.splice(productIdx, 1, { ...products, ...req.body});
+    res.status(201).send("Successfully Updated !!");
+});
+
+//DELETE Operations
+app.delete("/products/:productId", (req, res) => {
+    const productId = Number(req.params.productId);
+    const productIdx = products.findIndex((product) => product.id === productId);
+    products.splice(productIdx, 1);
+    res.status(201).json(product);
+});
+
+//DO NOT USE
+app.delete("/products", (req, res) => {
+    products = [];
+    res.status(201).send("Alert !! Everything Deleted ");
+});
+
+app.listen(8080, () => {
     console.log("server is started.You can access on localhost:8080");
 });
