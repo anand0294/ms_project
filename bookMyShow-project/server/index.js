57d11643b17e48e3c229c0013347b03436c7941b
anandmohantiwari94@gmail.com <anandmohantiwari94@gmail.com>
2024-09-21 00:35:40 +0530

    Movie Model and Movie Routes is added in the server

--- bookMyShow-project/server/index.js
+++ bookMyShow-project/server/index.js
@@ -6,10 +6,13 @@ require('dotenv').config(); // Load Environment Variables
 const dbConfig = require("./config/dbConfig");
 
 const userRoute = require("./routes/userRoutes");
+const movieRoutes = require("./routes/movieRoutes");
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/user', userRoute);
+app.use('/api/movie', movieRoutes);
 
 app.listen(5000, () => {
   console.log("Server is running on http://localhost:5000");
