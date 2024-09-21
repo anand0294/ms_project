38dee8bd192a6c22f02a6b46062155e9d6655773
anandmohantiwari94@gmail.com <anandmohantiwari94@gmail.com>
2024-09-21 21:46:04 +0530

    Movie Read and Movie Creation is Added and implemented inside the client & server

--- bookMyShow-project/client/src/apicalls/index.js
+++ bookMyShow-project/client/src/apicalls/index.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://localhost:3001",
   headers: {
     withCredentials: true,
-    method: 'post',
     'Content-Type': 'application/json',
-    // Authorization: `Bearer ${localStorage.getItem('token')}`
+    authorization: `Bearer ${localStorage.getItem('token')}`
   }
 });
