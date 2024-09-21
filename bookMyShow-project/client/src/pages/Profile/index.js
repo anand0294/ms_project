86a23b34bb9ad4531763c938475bba4d8e195ac4
anandmohantiwari94@gmail.com <anandmohantiwari94@gmail.com>
2024-09-21 22:40:21 +0530

    Theatre CRUD Operations Implementation

--- bookMyShow-project/client/src/pages/Profile/index.js
+++ bookMyShow-project/client/src/pages/Profile/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Tabs } from "antd";
-// import { useSelector, useDispatch } from "react-redux";
 import PageTitle from "../../components/PageTitle";
-// import TheatresList from "./TheatresList";
-//  import Bookings from "./Bookings";
+import TheatresList from "./TheatresList";
+
 function Profile() {
   return (
     <div>
@@ -13,8 +12,8 @@ function Profile() {
         <Tabs.TabPane tab="Bookings" key="1">
           <h1>This is Bookings</h1>
         </Tabs.TabPane>
-        <Tabs.TabPane tab="Apply for Theater" key="2">
-          <h1>Apply for theatres</h1>
+        <Tabs.TabPane tab="Apply for Theatre" key="2">
+          <TheatresList />
         </Tabs.TabPane>
       </Tabs>
     </div>
--- bookMyShow-project/server/index.js
+++ bookMyShow-project/server/index.js
@@ -7,12 +7,15 @@ const dbConfig = require("./config/dbConfig");
 
 const userRoute = require("./routes/userRoutes");
 const movieRoutes = require("./routes/movieRoutes");
+const theatreRoutes = require("./routes/theatreRoutes");
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/user', userRoute);
 app.use('/api/movie', movieRoutes);
+app.use('/api/theatre', theatreRoutes);
+
 
 app.listen(5000, () => {
   console.log("Server is running on http://localhost:5000");
